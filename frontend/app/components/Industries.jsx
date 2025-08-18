adacdef360c1aca0851e28fe822645f236005a4f
unknown <muhammedbinramli@gmail.com>
2025-08-18 14:50:30 +0530

    files changed industries. product

--- frontend/app/components/Industries.jsx
+++ frontend/app/components/Industries.jsx
@@ -1,33 +1,3 @@
-// import industries from "@/data/industries";
-
-// const Industries = () => {
-// return (
-//   <section className="section--primary" id="industries">
-//     <div className="inner--section">
-//       <div className="section--left ">
-//         <div className="section_head--primary">Industries We Serve</div>
-//       </div>
-//       <div className="section--right grid sm:grid-cols-2 gap-4">
-//         {industries.map((industry) => (
-//           <div
-//             key={industry.id}
-//             className="relative hover:-translate-y-[3px] active:translate-y-0 transition-transform cursor-pointer"
-//           >
-//             <img
-//               src={industry.url}
-//               alt={industry.image_attribute}
-//               className="h-[15rem] w-full sm:h-full rounded-[.5rem] object-cover"
-//             />
-//             <div className="image-text--primary">{industry.title}</div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   </section>
-// );
-// };
-
-// export default Industries;
 import Link from "next/link";
 import industries from "@/data/industries";
 
--- frontend/data/industries.js
+++ frontend/data/industries.js
@@ -1,33 +1,3 @@
-// const industries = [
-//   {
-//     id: 1,
-//     title: "Oil & Gas Segment",
-//     image_attribute: "Oil and Gas Segment Image",
-//     url: "/images/industries/oil1.png",
-//   },
-//   {
-//     id: 2,
-//     title: "Construction Sector",
-//     image: "Construction Sector Image",
-//     url: "/images/industries/construction.png",
-//   },
-//   {
-//     id: 3,
-//     title: "Marine Sector",
-//     image: "Marine Sector Image",
-//     url: "/images/industries/marine.png",
-//   },
-//   {
-//     id: 4,
-//     title: "Processing Industries",
-//     image: "Processing Industries Image",
-//     url: "/images/industries/processing.png",
-//   },
-// ];
-
-// export default industries;
-// app/data/industries.js
-// data/industries.js
 const industries = [
   {
     id: 1,
