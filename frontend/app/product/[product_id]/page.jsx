ea4bc49506d40473b7175e0afa4acc2fab23d9f0
Muhammed Aslam <muhammedbinramli@gmail.com>
2025-08-20 11:58:20 +0530

    made responsive product page

--- frontend/app/product/[product_id]/page.jsx
+++ frontend/app/product/[product_id]/page.jsx
@@ -15,13 +15,13 @@ const ProductPage = () => {
       <div className="w-[95%] lg:w-[85%] mx-auto">
         {product ? (
           <div className="space-y-12">
-            <div className="space-y-4">
+            <div className="md:space-y-2 lg:space-y-4">
               <h1 className="page--product-heading">{product.name}</h1>
               <div className="page--product-text">{product.description}</div>
             </div>
             <div className="space-y-2">
-              <div className="text-[1.2rem]">{`Our ${product.name} products :`}</div>
-              <div className="w-full grid grid-cols-4 gap-4">
+              <div className="text-[1rem] lg:text-[1.2rem]">{`Our ${product.name} products :`}</div>
+              <div className="w-full grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-3 md:gap-4">
                 {product.images.map((image, index) => (
                   <div
                     key={index}
@@ -30,9 +30,9 @@ const ProductPage = () => {
                     <img
                       src={image.image}
                       alt={image.title}
-                      className="w-[12rem] h-[10rem] object-contain"
+                      className="w-[9rem] h-[6rem] sm:w-[10rem] sm:h-[7rem] md:w-[9rem] xl:w-[12rem] md:h-[8rem] xl:h-[10rem] object-contain"
                     />
-                    <div className="text-center leading-[1.4rem] min-w-[20rem]">
+                    <div className="text-center text-[.9rem] md:text-[1rem] leading-[1.4rem] ">
                       {image.title}
                     </div>
                   </div>
