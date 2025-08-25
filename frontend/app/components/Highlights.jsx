a9de4b71f9ecf3951312c25d988abf66fcfbbf38
Muhammed Aslam <muhammedbinramli@gmail.com>
2025-08-25 15:44:54 +0530

    new structure

--- frontend/app/components/Highlights.jsx
+++ frontend/app/components/Highlights.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import highlights from "@/data/highlights";
 
 const Highlights = () => {
@@ -10,9 +12,9 @@ const Highlights = () => {
           </div>
         </div>
         <div className="section--right relative grid grid-cols-1 sm:grid-cols-3 sm:gap-4 mt-4 lg:mt-0">
-          {highlights.map((highlight) => (
-            <div key={highlight.id} className="highlight--card">
-              <div className="h-8 w-8 flex-shrink-0 rounded-full bg-neutral-400 circle-shift"></div>
+          {highlights.map(({ icon: Icon, ...highlight }) => (
+            <div key={highlight.id} className="highlight--card relative ">
+              <Icon className="text-[#0f243d] w-13 h-13" weight="regular" />
               <div>
                 <div className="text1--highlight-box">{highlight.title}</div>
                 <div className="text2--highlight-box">
--- frontend/data/highlights.js
+++ frontend/data/highlights.js
@@ -1,27 +1,31 @@
-import { GasPump } from "phosphor-react";
+import { Fire, Cube, Factory, Flask } from "phosphor-react";
 
 const highlights = [
   {
     id: 1,
     title: "Oil & Gas Expertise",
+    icon: Fire,
     description:
       "Specialized in high-performance sealing solutions for critical oilfield operations.",
   },
   {
     id: 2,
     title: "Wide Product Range",
+    icon: Cube,
     description:
       "From BOP seals to thread protectors—complete solutions for oilfield needs.",
   },
   {
     id: 3,
     title: "Advanced Manufacturing",
+    icon: Factory,
     description:
       "Equipped with modern rubber molding systems for precision and quality.",
   },
   {
     id: 4,
     title: "Proven Reliability",
+    icon: Flask,
     description:
       "Globally trusted for durable, field-tested performance in extreme conditions.",
   },
